197779805158a326c22616bb488303fa62c19565
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-02-09 17:31:42 +0530

    docs: ex-07: create an array of answers

--- triangleQuiz.js
+++ triangleQuiz.js
@@ -1,3 +1,5 @@
 const quizContainer = document.querySelector(".quiz-container");
 const submitAnswer = document.querySelector('button[type="submit"]');
 const output = document.querySelector("#output");
+
+const correctAnswers = ['90°', 'right angled'];
