c75f151e0b463757a64ca0aef891f4ff4b20b57b
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-02-09 19:44:40 +0530

    docs: ex-12: create a function

--- hypotenuse.js
+++ hypotenuse.js
@@ -1,3 +1,12 @@
-const sides = document.querySelectorAll(".input-side");
+const sides = document.querySelectorAll(".side-input");
 const hypotenuseBtn = document.querySelector("#hypotenuse-btn");
 const output = document.querySelector("#output");
+
+function calculateHypotenuse() {
+  const p = Number(sides[0].value);
+  const b = Number(sides[1].value);
+  const h = Math.floor(Math.sqrt(p * p + b * b));
+  console.log(h);
+}
+
+hypotenuseBtn.addEventListener("click", calculateHypotenuse);
