72fa958c2ec93961b450e59bb812c58499528707
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-02-09 17:28:27 +0530

    docs: ex-06: select the elements using querySelector

---
+++ triangleQuiz.js
@@ -0,0 +1,3 @@
+const quizContainer = document.querySelector(".quiz-container");
+const submitAnswer = document.querySelector('button[type="submit"]');
+const output = document.querySelector("#output");
