a45130cb940d7b01788a7f917d20d18d58781e6a
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-02-09 19:52:11 +0530

    docs: ex-13: display the output

--- hypotenuse.js
+++ hypotenuse.js
@@ -6,7 +6,7 @@ function calculateHypotenuse() {
   const p = Number(sides[0].value);
   const b = Number(sides[1].value);
   const h = Math.floor(Math.sqrt(p * p + b * b));
-  console.log(h);
+  output.innerText = `The length of the Hypotenuse = ${h}`;
 }
 
 hypotenuseBtn.addEventListener("click", calculateHypotenuse);
