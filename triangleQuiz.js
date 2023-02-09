49329267d1d15764cf47d888483dabfe9a14422f
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-02-09 18:31:50 +0530

    docs: ex-08: create a function

--- triangleQuiz.js
+++ triangleQuiz.js
@@ -1,5 +1,23 @@
-const quizContainer = document.querySelector(".quiz-container");
-const submitAnswer = document.querySelector('button[type="submit"]');
+const quizForm = document.querySelector(".quiz-form");
+const submitAnswerBtn = document.querySelector('button[type="submit"]');
 const output = document.querySelector("#output");
 
-const correctAnswers = ['90°', 'right angled'];
+const correctAnswers = ["90°", "right angled"];
+
+function calculateScore() {
+  let score = 0;
+  let index = 0;
+  const formResults = new FormData(quizForm);
+  for (let value of formResults.values()) {
+    if(value == correctAnswers[index]){
+        score++;
+    }
+    index++;
+  }
+  console.log(score);
+}
+
+submitAnswerBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  calculateScore();
+});
